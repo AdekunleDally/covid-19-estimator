02be14dffab3ab34d7c7de03c738909cadecda36
ADEOKUN LUKMAN <adekunledally@gmail.com>
2020-04-11 18:04:24 +0100

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -35,7 +35,7 @@ const covid19ImpactEstimator = (data) => {
   impact.infectionsByRequestedTime = impact.currentlyInfected * 1024;
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 1024;
   if (data.periodType === 'days') {
-    severeImpact.infecPerDay = severeImpact.currentlyInfected * 2 ** (Math.trunc(elapsedTime / 30));
+    severeImpact.infecPerDay = severeImpact.currentlyInfected * 2 ** (Math.trunc(elapsedTime / 3));
     impact.infecPerDay = impact.currentlyInfected * 2 ** (Math.trunc(elapsedTime / 30));
     return (impact.infecPerDay, severeImpact.infecPerDay);
   } if (data.periodType === 'weeks') {
