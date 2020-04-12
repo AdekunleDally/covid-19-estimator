05919a1119df77947f2a0c83ad72fc5aa7f5c3a9
ADEOKUN LUKMAN <adekunledally@gmail.com>
2020-04-12 10:16:16 +0100

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -29,41 +29,41 @@ const impact = {
   };
   
   // eslint-disable-next-line no-shadow
-  const covid19ImpactEstimator = (data) => {
-    const input = data;
-    const elapsedTime = data.timeToElapse;
-    let elapse;
-    impact.currentlyInfected = input.reportedCases * 10;
-    severeImpact.currentlyInfected = input.reportedCases * 50;
-    // impact.infectionsByRequestedTime = impact.currentlyInfected * 1024;
-    // severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 1024;
-    switch (data.periodType) {
-      case 'months':
-        elapse = Math.trunc((elapsedTime / 3) * 30);
-        break;
-      case 'weeks':
-        elapse = Math.trunc(elapsedTime / 3) * 7;
-        break;
-      default:
-        elapse = Math.trunc(elapsedTime / 3);
-        break;
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  const elapsedTime = data.timeToElapse;
+  let elapse;
+  impact.currentlyInfected = input.reportedCases * 10;
+  severeImpact.currentlyInfected = input.reportedCases * 50;
+  // impact.infectionsByRequestedTime = impact.currentlyInfected * 1024;
+  // severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 1024;
+  switch (data.periodType) {
+    case 'months':
+    elapse = Math.trunc((elapsedTime / 3) * 30);
+    break;
+    case 'weeks':
+    elapse = Math.trunc(elapsedTime / 3) * 7;
+    break;
+    default:
+    elapse = Math.trunc(elapsedTime / 3);
+    break;
+  }
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** elapse);
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** elapse);
+  return {
+    data: input,
+    impact: {
+      currentlyInfected: impact.currentlyInfected,
+      infectionsByRequestedTime: impact.infectionsByRequestedTime,
+      severeCasesByRequestTime: impact.severeCasesByRequestTime
+    },
+    severeImpact: {
+      currentlyInfected: severeImpact.currentlyInfected,
+      infectionsByRequestedTime: severeImpact.infectionsByRequestedTime,
+      severeCasesByRequestTime: severeImpact.severeCasesByRequestTime
     }
-    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** elapse);
-    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** elapse);
-    return {
-      data: input,
-      impact: {
-        currentlyInfected: impact.currentlyInfected,
-        infectionsByRequestedTime: impact.infectionsByRequestedTime,
-        severeCasesByRequestTime: impact.severeCasesByRequestTime
-      },
-      severeImpact: {
-        currentlyInfected: severeImpact.currentlyInfected,
-        infectionsByRequestedTime: severeImpact.infectionsByRequestedTime,
-        severeCasesByRequestTime: severeImpact.severeCasesByRequestTime
-      }
-    };
   };
-  covid19ImpactEstimator(data);
-  // export default covid19ImpactEstimator;
+};
+covid19ImpactEstimator(data);
+// export default covid19ImpactEstimator;
   
