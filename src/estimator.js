eff2fb087677095763391a279376451228926c6a
ADEOKUN LUKMAN <adekunledally@gmail.com>
2020-04-12 12:13:13 +0100

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,79 +1,79 @@
 const impact = {
-    currentlyInfected: 0,
-    infectionsByRequestedTime: 0,
-    severeCasesByRequestTime: 0,
-    hospitalBedsByRequestedTime: 0,
-    infecPerDay: 0,
-    infecPerWeek: 0,
-    infectedPeopleOver30Days: 0
-  };
-  const severeImpact = {
-    currentlyInfected: 0,
-    infectionsByRequestedTime: 0,
-    severeCasesByRequestTime: 0,
-    hospitalBedsByRequestedTime: 0,
-    infecPerDay: 0,
-    infecPerWeek: 0,
-    infectedPeopleOver30Days: 0
-  };
-  const data = {
-    region: {
-      name: 'Africa',
-      avgAge: 19.7,
-      avgDailyIncomeInUSD: 5,
-      avgDailyIncomePopulation: 0.71
+  currentlyInfected: 0,
+  infectionsByRequestedTime: 0,
+  severeCasesByRequestTime: 0,
+  hospitalBedsByRequestedTime: 0,
+  infecPerDay: 0,
+  infecPerWeek: 0,
+  infectedPeopleOver30Days: 0
+};
+const severeImpact = {
+  currentlyInfected: 0,
+  infectionsByRequestedTime: 0,
+  severeCasesByRequestTime: 0,
+  hospitalBedsByRequestedTime: 0,
+  infecPerDay: 0,
+  infecPerWeek: 0,
+  infectedPeopleOver30Days: 0
+};
+const data = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
+// eslint-disable-next-line no-shadow
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  const elapsedTime = data.timeToElapse;
+  let elapse;
+  impact.currentlyInfected = input.reportedCases * 10;
+  severeImpact.currentlyInfected = input.reportedCases * 50;
+  // impact.infectionsByRequestedTime = impact.currentlyInfected * 1024;
+  // severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 1024;
+  switch (data.periodType) {
+    case 'months':
+      elapse = Math.trunc((elapsedTime / 3) * 30);
+      break;
+    case 'weeks':
+      elapse = Math.trunc((elapsedTime / 3) * 7);
+      break;
+    default:
+      elapse = Math.trunc(elapsedTime / 3);
+      break;
+  }
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** elapse);
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** elapse);
+  const occupiedBedSpaces = 0.65 * data.totalHospitalBeds;
+  const availableBedSpaces = data.totalHospitalBeds - occupiedBedSpaces;
+  const expectedBedAvailabilityImpact = 0.35 * impact.severeCasesByRequestTime;
+  const expectedBedAvailabilitySevereImp = 0.35 * severeImpact.severeCasesByRequestTime;
+  const a = availableBedSpaces - expectedBedAvailabilityImpact;
+  impact.hospitalBedsByRequestedTime = Math.trunc(a);
+  const b = availableBedSpaces - expectedBedAvailabilitySevereImp;
+  severeImpact.hospitalBedsByRequestedTime = Math.trunc(b);
+  return {
+    data: input,
+    impact: {
+      currentlyInfected: impact.currentlyInfected,
+      infectionsByRequestedTime: impact.infectionsByRequestedTime,
+      severeCasesByRequestTime: impact.severeCasesByRequestTime,
+      hospitalBedsByRequestedTime: impact.hospitalBedsByRequestedTime
     },
-    periodType: 'days',
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-  };
-  // eslint-disable-next-line no-shadow
-  const covid19ImpactEstimator = (data) => {
-    const input = data;
-    const elapsedTime = data.timeToElapse;
-    let elapse;
-    impact.currentlyInfected = input.reportedCases * 10;
-    severeImpact.currentlyInfected = input.reportedCases * 50;
-    // impact.infectionsByRequestedTime = impact.currentlyInfected * 1024;
-    // severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 1024;
-    switch (data.periodType) {
-      case 'months':
-        elapse = Math.trunc((elapsedTime / 3) * 30);
-        break;
-      case 'weeks':
-        elapse = Math.trunc((elapsedTime / 3) * 7);
-        break;
-      default:
-        elapse = Math.trunc(elapsedTime / 3);
-        break;
+    severeImpact: {
+      currentlyInfected: severeImpact.currentlyInfected,
+      infectionsByRequestedTime: severeImpact.infectionsByRequestedTime,
+      severeCasesByRequestTime: severeImpact.severeCasesByRequestTime,
+      hospitalBedsByRequestedTime: severeImpact.hospitalBedsByRequestedTime
     }
-    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** elapse);
-    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** elapse);
-    const occupiedBedSpaces = 0.65 * data.totalHospitalBeds;
-    const availableBedSpaces = data.totalHospitalBeds - occupiedBedSpaces;
-    const expectedBedAvailabilityImpact = 0.35 * impact.severeCasesByRequestTime;
-    const expectedBedAvailabilitySevereImp = 0.35 * severeImpact.severeCasesByRequestTime;
-    const a = availableBedSpaces - expectedBedAvailabilityImpact;
-    impact.hospitalBedsByRequestedTime = Math.trunc(a);
-    const b = availableBedSpaces - expectedBedAvailabilitySevereImp;
-    severeImpact.hospitalBedsByRequestedTime = Math.trunc(b);
-    return {
-      data: input,
-      impact: {
-        currentlyInfected: impact.currentlyInfected,
-        infectionsByRequestedTime: impact.infectionsByRequestedTime,
-        severeCasesByRequestTime: impact.severeCasesByRequestTime,
-        hospitalBedsByRequestedTime: impact.hospitalBedsByRequestedTime
-      },
-      severeImpact: {
-        currentlyInfected: severeImpact.currentlyInfected,
-        infectionsByRequestedTime: severeImpact.infectionsByRequestedTime,
-        severeCasesByRequestTime: severeImpact.severeCasesByRequestTime,
-        hospitalBedsByRequestedTime: severeImpact.hospitalBedsByRequestedTime
-      }
-    };
   };
-  covid19ImpactEstimator(data);
-  export default covid19ImpactEstimator;
+};
+covid19ImpactEstimator(data);
+export default covid19ImpactEstimator;
